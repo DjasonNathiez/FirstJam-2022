01aabd3410e163f034119fdede68fa9e597d83b0
E <t.marcuzzi@rubika-edu.com>
2022-11-11 14:31:05 +0100

    m

--- KAöS/Assets/CoreManager.cs
+++ KAöS/Assets/CoreManager.cs
@@ -1,14 +1,35 @@
+using System;
 using Photon.Pun;
 using UnityEngine;
 
 public class CoreManager : MonoBehaviour,IDamageable, IPunObservable
 {
+    public static CoreManager Instance;
+
+    private Rigidbody2D rb;
     public int currentLife;
-    
+
+    private void Awake()
+    {
+        if (Instance != null) return;
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Impulse(Vector2 dir, float force)
+    {
+        rb.AddForce(dir*force);
+    }
+
     public void TakeDamage(int amount)
     {
         currentLife -= amount;
     }
+    
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
